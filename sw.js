9de1ba165bcae015fe0aab01de0f1d2ea0743417
crystalheart636-source <crystal.heart636@gmail.com>
2026-07-03 09:37:11 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'expense-v17';
+
+self.addEventListener('install', function (e) {
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(['./', './index.html']);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keys) {
+            return Promise.all(
+                keys.filter(function (key) { return key !== CACHE_NAME; })
+                    .map(function (key) { return caches.delete(key); })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
